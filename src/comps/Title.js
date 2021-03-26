fc0c704bd39f22b1d37d28d137bd3b3774f72d9f
Mo Rob <31892724+morob1@users.noreply.github.com>
2021-03-26 17:00:21 +0000

    update text

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>FireGram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Upload some images to view them in the gallery below.</p>
     </div>
   )
 }
